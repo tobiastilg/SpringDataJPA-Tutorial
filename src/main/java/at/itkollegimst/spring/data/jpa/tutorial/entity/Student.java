dd8ccc3a6498a015544631f124e855589d3117ea
tobiastilg <tobi.tilg@gmail.com>
2021-10-30 13:26:38 +0200

    added student entity

---
+++ src/main/java/at/itkollegimst/spring/data/jpa/tutorial/entity/Student.java
@@ -0,0 +1,40 @@
+package at.itkollegimst.spring.data.jpa.tutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "tbl_student", //Tabellenname mitgeben (alter table wird nicht gelöscht -> ddl-auto=update )
+       uniqueConstraints = @UniqueConstraint( //UniqueKeys festlegen
+               name = "emailid_unique",
+               columnNames = "email_address"))
+public class Student {
+
+    @Id //auto-increment/PrimaryKey
+    @SequenceGenerator(name = "student_sequence", //definiert den PrimaryKey-Generator
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    /*SEQUENCE erzeugt den Wert schon vor der Erstellung der Entity (Vorteil gegenüber AUTO oder IDENTITY),
+    wichtig sollte der PrimaryKey schon zuvor benötigt wird (bessere Lösung)
+    Dokumentation: https://www.objectdb.com/java/jpa/entity/generated#the_sequence_strategy*/
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, //Spezifizierung von PrimaryKey Generierung
+            generator = "student_sequence")
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column(name = "email_address", //Spaltenname mitgeben (sollte bei jedem datenfeld gemacht werden)
+            nullable = false) //darf nicht null sein
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
